201de61b6395079853618d3f8c36e77098c97819
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-06-25 18:00:27 +0530

    getting ready for tourney 2

--- script/agents/SandwichGold.sol
+++ script/agents/SandwichGold.sol
@@ -6,38 +6,12 @@ import "forge-std/console.sol";
 
 // Player that buys whatever good we can get the most of each round.
 contract SandwichGold is BasePlayer {
-    uint8 finalAssetIndex;
-
     constructor(
         IGame game,
         uint8 playerIdx,
         uint8 playerCount,
         uint8 assetCount
     ) BasePlayer(game, playerIdx, playerCount, assetCount) {}
-
-    function maxNum(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a > b ? a : b;
-    }
-
-    function minNum(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
-    // Find the good (non-gold asset) we have the lowest balance of and not 0.
-    function _getMinNonZeroGood() internal view returns (uint8 minAssetIdx) {
-        minAssetIdx = FIRST_GOOD_IDX;
-        uint256 minBalance = GAME.balanceOf(PLAYER_IDX, FIRST_GOOD_IDX);
-        for (uint8 i = 1; i < GOODS_COUNT; ++i) {
-            uint8 assetIdx = FIRST_GOOD_IDX + i;
-            uint256 bal = GAME.balanceOf(PLAYER_IDX, assetIdx);
-            if (bal > 0) {
-                if (minBalance >= bal) {
-                    minAssetIdx = assetIdx;
-                    minBalance = bal;
-                }
-            }
-        }
-    }
 
     function _getCheapestAssetForGold(
         uint256 goldAmount
@@ -62,27 +36,50 @@ contract SandwichGold is BasePlayer {
     }
 
     function createBundle(
-        uint8 /* builderIdx */
+        uint8 builderIdx
     ) external virtual override returns (PlayerBundle memory bundle) {
-        if (GAME.round() == 32) {
-            bundle.swaps = new SwapSell[](1);
-            (uint8 assetIndexMax, ) = _getCheapestAssetForGold(
-                GAME.balanceOf(PLAYER_IDX, GOLD_IDX)
-            );
+        // TODO: Add check here that sells everything for 1 asset if this is round 32
+        if (builderIdx % 4 == 0) {
+            require(false, "I have no bundle");
+        } else if (builderIdx % 4 == 1) {
+            bundle = PlayerBundle({swaps: new SwapSell[](1)});
             bundle.swaps[0] = SwapSell({
                 fromAssetIdx: GOLD_IDX,
-                toAssetIdx: assetIndexMax,
+                toAssetIdx: FIRST_GOOD_IDX,
+                fromAmount: GAME.balanceOf(PLAYER_IDX, GOLD_IDX) + 1000
+            });
+        } else if (builderIdx % 4 == 2) {
+            bundle = PlayerBundle({swaps: new SwapSell[](1)});
+            bundle.swaps[0] = SwapSell({
+                fromAssetIdx: GOLD_IDX,
+                toAssetIdx: 100,
                 fromAmount: GAME.balanceOf(PLAYER_IDX, GOLD_IDX)
             });
         } else {
-            bundle.swaps = new SwapSell[](GOODS_COUNT);
-            for (uint8 i; i < GOODS_COUNT; ++i) {
+            uint8 first = FIRST_GOOD_IDX;
+            uint8 second = FIRST_GOOD_IDX + 1;
+
+            uint256 initialFirst = GAME.balanceOf(PLAYER_IDX, first);
+            uint256 initialSecond = GAME.balanceOf(PLAYER_IDX, second);
+
+            bundle = PlayerBundle({swaps: new SwapSell[](12)});
+
+            for (uint256 i; i < bundle.swaps.length; i++) {
                 bundle.swaps[i] = SwapSell({
-                    fromAssetIdx: FIRST_GOOD_IDX + i,
-                    toAssetIdx: GOLD_IDX,
-                    fromAmount: GAME.balanceOf(PLAYER_IDX, FIRST_GOOD_IDX + i)
+                    fromAssetIdx: i % 2 == 0 ? first : second,
+                    toAssetIdx: i % 2 == 0 ? second : first,
+                    fromAmount: GAME.balanceOf(
+                        PLAYER_IDX,
+                        i % 2 == 0 ? first : second
+                    )
                 });
             }
+
+            require(
+                initialFirst <= GAME.balanceOf(PLAYER_IDX, first) &&
+                    initialSecond <= GAME.balanceOf(PLAYER_IDX, second),
+                "Loss"
+            );
         }
     }
 
@@ -105,21 +102,35 @@ contract SandwichGold is BasePlayer {
         uint256 initialGold = GAME.balanceOf(PLAYER_IDX, GOLD_IDX);
         int256[] memory assetAmounts = new int256[](ASSET_COUNT);
 
+        bytes memory testNull = abi.encode(bundle);
+        bool salmonellaBundle = false;
+
+        if (testNull.length == 0) {
+            GAME.settleBundle(playerIdx, bundle);
+            return 0;
+        }
+
         for (uint256 i; i < bundle.swaps.length; i++) {
             SwapSell memory swap = bundle.swaps[i];
-
-            assetAmounts[swap.toAssetIdx] += int256(
+            try
                 GAME.quoteSell(
                     swap.fromAssetIdx,
                     swap.toAssetIdx,
                     swap.fromAmount
                 )
-            );
-
-            assetAmounts[swap.fromAssetIdx] -= int256(swap.fromAmount);
+            returns (uint256 toAmount) {
+                assetAmounts[swap.toAssetIdx] += int256(toAmount);
+                assetAmounts[swap.fromAssetIdx] -= int256(swap.fromAmount);
+            } catch {
+                salmonellaBundle = true;
+            }
         }
-
         uint8 assetIndexMax = uint8(findMaxInList(assetAmounts));
+
+        if (salmonellaBundle) {
+            GAME.settleBundle(playerIdx, bundle);
+            return 0;
+        }
 
         GAME.sell(
             GOLD_IDX,
@@ -134,8 +145,6 @@ contract SandwichGold is BasePlayer {
             GOLD_IDX,
             GAME.balanceOf(PLAYER_IDX, assetIndexMax)
         );
-
-        goldProfit = GAME.balanceOf(PLAYER_IDX, GOLD_IDX) - initialGold;
     }
 
     function buildBlock(
@@ -143,6 +152,7 @@ contract SandwichGold is BasePlayer {
     ) external virtual override returns (uint256 goldBid) {
         uint256 bidAmount = 0;
         uint256 goldReceivedIfFirst = 0;
+        uint256 totalSandwichProfit = 0;
 
         (uint8 assetIndexMax, uint256 assetAmount) = _getCheapestAssetForGold(
             GAME.balanceOf(PLAYER_IDX, GOLD_IDX)
@@ -172,7 +182,7 @@ contract SandwichGold is BasePlayer {
                 );
             }
 
-            uint256 totalSandwichProfit = 0;
+            uint256 initialGold = GAME.balanceOf(PLAYER_IDX, GOLD_IDX);
 
             // Settle everyone else's bundles with sandwiches
             for (uint8 playerIdx = 0; playerIdx < bundles.length; ++playerIdx) {
@@ -180,10 +190,7 @@ contract SandwichGold is BasePlayer {
                     // Skip our bundle.
                     continue;
                 }
-                totalSandwichProfit += sandwichBundleWithGold(
-                    playerIdx,
-                    bundles[playerIdx]
-                );
+                sandwichBundleWithGold(playerIdx, bundles[playerIdx]);
             }
 
             // Check if at the end of the block we can win
@@ -196,7 +203,23 @@ contract SandwichGold is BasePlayer {
                 GAME.buy(GOLD_IDX, assetIndexMax, 64e18);
                 bidAmount = GAME.balanceOf(PLAYER_IDX, GOLD_IDX);
             } else {
-                bidAmount = (totalSandwichProfit * 90) / 100;
+                uint256 totalProfit = GAME.balanceOf(PLAYER_IDX, GOLD_IDX) >
+                    initialGold
+                    ? GAME.balanceOf(PLAYER_IDX, GOLD_IDX) - initialGold
+                    : 0;
+
+                if (totalProfit == 0) {
+                    bidAmount = 2e14;
+                } else if (GAME.round() == 32) {
+                    GAME.sell(
+                        GOLD_IDX,
+                        assetIndexMax,
+                        (GAME.balanceOf(PLAYER_IDX, GOLD_IDX) * 9) / 10
+                    );
+                    bidAmount = GAME.balanceOf(PLAYER_IDX, GOLD_IDX);
+                } else {
+                    // bidAmount = (totalProfit * 99) / 100;
+                }
             }
         }
 
